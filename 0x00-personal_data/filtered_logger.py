eb5513afd231a594636d53a853c7d5360ccba7d8
sarahassan0 <sarraree5@gmail.com>
2024-02-09 03:15:28 +0200

    get_db function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
@@ -25,6 +27,22 @@ def get_logger() -> logging.Logger:
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ returns a connector to a database """
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or 'localhost'
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or 'root'
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ''
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=database
+    )
 
 
 class RedactingFormatter(logging.Formatter):
