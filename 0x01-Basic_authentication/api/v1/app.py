132035d9ec652091e4ebd096a3abaa44af23857a
sarahassan0 <sarraree5@gmail.com>
2024-02-14 01:11:02 +0200

    feat: request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,30 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+auth = None
+if os.getenv("AUTH_TYPE") == "auth":
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """before_request handler"""
+    if auth is None:
+        return
+
+    excluded_paths = [
+        "/api/v1/status/",
+        "/api/v1/unauthorized/",
+        "/api/v1/forbidden/",
+    ]
+    if auth.require_auth(request.path, excluded_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,12 +14,18 @@ class Auth:
             require_auth method
             returns true if path requires authentication else false.
         """
-        return False
+        if path and excluded_paths and len(excluded_paths) > 0:
+            if path in excluded_paths or path + "/" in excluded_paths or path[0:-1] in excluded_paths:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
             authorization header method
         """
+        if request and "Authorization" in request.headers:
+            return request.headers["Authorization"]
         return None
 
     def current_user(self, request=None) -> TypeVar("User"):  # type: ignore
@@ -27,3 +33,14 @@ class Auth:
             get current_user method
         """
         return None
+
+
+a = Auth()
+
+print(a.require_auth(None, None))
+print(a.require_auth(None, []))
+print(a.require_auth("/api/v1/status/", []))
+print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
+print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
+print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
+print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
