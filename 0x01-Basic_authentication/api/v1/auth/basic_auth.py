b3c8332180484c2db8a55ddffa6ac4b8e71ddd35
sarahassan0 <sarraree5@gmail.com>
2024-02-15 01:36:56 +0200

    edit: update `decode_base64_authorization_header`

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,7 +36,7 @@ class BasicAuth(Auth):
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             return decoded_bytes.decode('utf-8')
-        except:
+        except base64.binascii.Error as e:
             return None
 
     def extract_user_credentials(
