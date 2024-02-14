3d5d8553ab57756702c21e5dd6b149d71b345058
sarahassan0 <sarraree5@gmail.com>
2024-02-15 01:09:47 +0200

    feat: create `decode_base64_authorization_header`

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 """
 
 from .auth import Auth
+import base64
+
 
 
 class BasicAuth(Auth):
@@ -22,3 +24,20 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+    
+    
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """decode base64 header"""
+        if (base64_authorization_header is None
+                or type(base64_authorization_header) is not str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except UnicodeDecodeError:
+            return None
+        
