d6d599a14b934d26cff961dd352879b78b81e516
sarahassan0 <sarraree5@gmail.com>
2024-02-15 16:53:48 +0200

    feat: update `seesionAuth ` class by adding `create_session` func

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,28 @@
 """
 
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ SessionAuth Class"""
+    user_id_by_session_id = {}
+
     def __init__(self) -> None:
-        """ initalize SessionAuth class"""
+        """
+            initalize SessionAuth class
+        """
+
         super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            Creates a Session ID for a user_id:
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
