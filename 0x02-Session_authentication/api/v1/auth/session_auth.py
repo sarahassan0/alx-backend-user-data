633a45b482c788fcd66de236dd38664ee702e2a4
sarahassan0 <sarraree5@gmail.com>
2024-02-17 20:04:06 +0200

    feat: update `seesionAuth ` add`current_user` func

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@
 
 from .auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -32,9 +33,27 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
-            Creates a Session ID for a user_id:
+            Creates a Session ID for a user_id
         """
         if session_id is None or not isinstance(session_id, str):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+            Returns a User instance based on a cookie value
+        """
+        if request is None:
+            return None
+
+        SESSION_NAME = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(SESSION_NAME)
+
+        if user_id is None:
+            return None
+        user = User.get(user_id)
+        if user is None:
+            return None
+
+        return user
