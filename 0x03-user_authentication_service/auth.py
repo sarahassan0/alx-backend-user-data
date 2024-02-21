7b96b7f7251195f06ea89552c0a38049509ca1ea
sarahassan0 <sarraree5@gmail.com>
2024-02-22 01:38:07 +0200

    feat: implement `create_session` & `valid_login` methods

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 class Auth:
@@ -26,9 +27,33 @@ class Auth:
                                      hashed_password=hashed_password)
             return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate user's email and password to login it """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Validate user's email and password to login it """
+        try:
+            user = self._db.find_user_by(email=email)
+            user.session_id = _generate_uuid()
+            self._db._session.commit()
+            return user.session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes user's password """
     password_bytes = password.encode('utf-8')
     hashed_pass = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
     return hashed_pass
+
+
+def _generate_uuid() -> str:
+    """Generates a new UUID"""
+    return str(uuid.uuid4())
