1a41bd88e571bec9f7f1f5bd27cdc2e2106fb0c0
sarahassan0 <sarraree5@gmail.com>
2024-02-22 01:01:50 +0200

    feat: add POST `user` route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,20 +2,36 @@
 """
 Route module for the API
 """
-from os import getenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import (CORS, cross_origin)
-import os
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
+
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """ main route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ Register a user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
