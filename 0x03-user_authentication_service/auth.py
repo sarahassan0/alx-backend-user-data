9ea1ac56979b1e1a06e92f607829cc0f603fc926
sarahassan0 <sarraree5@gmail.com>
2024-02-22 23:40:00 +0200

    feat: implement  `Auth.get_user_from_session_id`  & `Auth.destroy_session` methods

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,6 +46,27 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Returns user when it has a valid session_id"""
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Removes user's session"""
+        if not user_id:
+            return None
+
+        try:
+            user = self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Hashes user's password """
